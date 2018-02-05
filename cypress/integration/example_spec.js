d02d56c55a2c95de68cee5d103b28ebed33e211d
nsolimine <32491766+nsolimine@users.noreply.github.com>
2018-02-05 15:47:19 -0700

    testing

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -2,15 +2,8 @@ describe("chordBae", function () {
   it("works", function () {
     cy.visit("/");
     cy.get("h1").should("have.text", "chordBae");
-    cy.get("select").should("have.length", 8);
-    cy.get("@listItems").eq(0).find("a").should("have.text", "Demo");
-    cy.get("@listItems").eq(1).find("a").should("have.text", "Order");
-    cy.get("@listItems").eq(2).find("span").should("have.text", "Product");
-    cy.get("@listItems").eq(2).find("ul > li").as("listItems2");
-    cy.get("@listItems2").eq(1).should("have.text", "Documentation");
-    cy.get("@listItems2").eq(2).should("have.text", "Features");
-    cy.get("@listItems2").eq(3).should("have.text", "Technology");
-    cy.get("body > section > ul").find("h4").should("have.length", 6);
-    cy.get("a").contains("Order").click();
+    cy.get("option").should("have.length", 16);
+    cy.get("#chordsDiv").should("have.length", 1);
+    cy.get("header > div > p").contains("Pick a key, either Major or Minor");
   });
 });
